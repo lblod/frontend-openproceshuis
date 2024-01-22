9b197a977db73bc4736f50def30f3077fbc4dbf9
Martijn Bogaert <martijn.bogaert@sirus.be>
2024-01-22 23:05:25 +0100

    Add alert on file upload

--- app/controllers/bpmn-files/index.js
+++ app/controllers/bpmn-files/index.js
@@ -1,6 +1,7 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import config from 'frontend-processendatabank/config/environment';
 
 export default class BpmnFilesIndexController extends Controller {
   queryParams = ['page', 'name'];
@@ -8,6 +9,8 @@ export default class BpmnFilesIndexController extends Controller {
   @tracked page = 0;
   size = 20;
   @tracked name = '';
+  @tracked fileModalOpened = false;
+  @tracked newFileId = undefined;
 
   get bpmnFiles() {
     return this.model.loadBpmnFilesTaskInstance.isFinished
@@ -30,17 +33,45 @@ export default class BpmnFilesIndexController extends Controller {
     return this.model.loadBpmnFilesTaskInstance.isError;
   }
 
+  get postEndpoint() {
+    return `${config.APP.apiHost}/bpmn-files`;
+  }
+
   @action
   setName(selection) {
     this.page = null;
     this.name = selection;
   }
 
-  @action resetFilters() {
+  @action
+  resetFilters() {
     this.name = '';
     this.page = 0;
 
     // Triggers a refresh of the model
     this.page = null;
+  }
+
+  @action
+  openFileModal() {
+    this.newFileId = undefined;
+    this.fileModalOpened = true;
+  }
+
+  @action
+  closeFileModal() {
+    this.fileModalOpened = false;
+  }
+
+  @action
+  fileUploaded(newFileId) {
+    this.closeFileModal();
+    this.resetFilters();
+    this.newFileId = newFileId;
+  }
+
+  @action
+  closeAlert() {
+    this.newFileId = undefined;
   }
 }
