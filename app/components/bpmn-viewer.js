ce5b24796a143232870e07f1c4fc8d4af55fbb61
Martijn Bogaert <martijn.bogaert@sirus.be>
2024-01-25 09:27:00 +0100

    Style bpmn container

--- app/components/bpmn-viewer.js
+++ app/components/bpmn-viewer.js
@@ -1,11 +1,11 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
-import BpmnViewer from 'bpmn-js';
+import NavigatedViewer from 'bpmn-js/lib/NavigatedViewer';
 
 export default class BpmnViewerComponent extends Component {
   @action
   async setupViewer(element) {
-    const viewer = new BpmnViewer({ container: element });
+    const viewer = new NavigatedViewer({ container: element });
 
     const bpmnXml = this.args.bpmnXml;
     if (bpmnXml) {
