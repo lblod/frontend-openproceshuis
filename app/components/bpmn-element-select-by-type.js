c511a5e1efcf8799a25bb580c3688c5d455dfc90
Martijn Bogaert <martijn.bogaert@sirus.be>
2024-01-31 15:13:04 +0100

    Populate type filter list automatically

--- app/components/bpmn-element-select-by-type.js
+++ app/components/bpmn-element-select-by-type.js
@@ -2,13 +2,9 @@ import Component from '@glimmer/component';
 import { BpmnElementTypes } from '../utils/bpmn-element-types';
 
 export default class BpmnElementSelectByTypeComponent extends Component {
-  types = [
-    BpmnElementTypes.BusinessRuleTask,
-    BpmnElementTypes.ManualTask,
-    BpmnElementTypes.ReceiveTask,
-    BpmnElementTypes.ScriptTask,
-    BpmnElementTypes.SendTask,
-    BpmnElementTypes.Task,
-    BpmnElementTypes.UserTask,
-  ].sort();
+  get types() {
+    return Object.values(BpmnElementTypes)
+      .filter((type) => type !== BpmnElementTypes.BpmnElement)
+      .sort();
+  }
 }
