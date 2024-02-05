b51a46235341f7ac91741576bf1039203ac657bf
Martijn Bogaert <martijn.bogaert@sirus.be>
2024-02-05 11:37:37 +0100

    Change app name to Open Proces Huis

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from 'frontend-processendatabank/app';
-import config from 'frontend-processendatabank/config/environment';
+import Application from 'frontend-openproceshuis/app';
+import config from 'frontend-openproceshuis/config/environment';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
