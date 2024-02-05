46dc075eb52b34c981c24816b6c63f1469e92a2e
Nordine Bittich <nordine@bittich.be>
2024-02-05 17:02:34 +0100

    show progress

--- app/controllers/bpmn-files/index.js
+++ app/controllers/bpmn-files/index.js
@@ -2,6 +2,7 @@ import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
+import { task } from 'ember-concurrency';
 export default class BpmnFilesIndexController extends Controller {
   queryParams = ['page', 'name'];
 
@@ -62,15 +63,17 @@ export default class BpmnFilesIndexController extends Controller {
     this.fileModalOpened = false;
   }
 
-  @action
-  async fileUploaded(newFileId) {
-    await fetch(`/bpmn?id=${newFileId}`, {
+  @task({ enqueue: true, maxConcurrency: 3 })
+  *extractBpmn(newFileId) {
+    yield fetch(`/bpmn?id=${newFileId}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/vnd.api+json',
       },
     });
-
+  }
+  @action
+  fileUploaded(newFileId) {
     this.closeFileModal();
     this.resetFilters();
     this.newFileId = newFileId;
