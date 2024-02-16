a7cb739b308f67b0fdc60a35d691d56b31b0ca9c
Martijn Bogaert <martijn.bogaert@sirus.be>
2024-02-16 11:08:16 +0100

    Get rid of unnecessary application adapter logic

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,12 +1,3 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
 
-export default class ApplicationAdapter extends JSONAPIAdapter {
-  pathForType(type) {
-    let path = super.pathForType(type);
-
-    if (!['file', 'bpmn-element'].includes(type))
-      path = `bpmn-elements/${path}`;
-
-    return path;
-  }
-}
+export default class ApplicationAdapter extends JSONAPIAdapter {}
