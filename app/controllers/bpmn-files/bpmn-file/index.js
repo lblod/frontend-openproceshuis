e4310d92812ae499b84ed3e765383faec6055f4f
Martijn Bogaert <martijn.bogaert@sirus.be>
2024-02-28 17:36:33 +0100

    Bring back old session behaviour

--- app/controllers/bpmn-files/bpmn-file/index.js
+++ app/controllers/bpmn-files/bpmn-file/index.js
@@ -1,12 +1,9 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { service } from '@ember/service';
 
 export default class BpmnFilesBpmnFileIndexController extends Controller {
   queryParams = ['page', 'size', 'sort'];
-
-  @service currentSession;
 
   @tracked page = 0;
   size = 20;
--- app/controllers/bpmn-files/index.js
+++ app/controllers/bpmn-files/index.js
@@ -1,11 +1,8 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { service } from '@ember/service';
 
 export default class BpmnFilesIndexController extends Controller {
-  @service currentSession;
-
   queryParams = ['page', 'size', 'sort', 'name'];
 
   @tracked page = 0;
