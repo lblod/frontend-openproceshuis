03121335ae92ebcade34b44bd0d0e7097bcaea62
Martijn Bogaert <martijn.bogaert@sirus.be>
2024-03-04 16:59:35 +0100

    Add description attribute to file model

--- app/models/file.js
+++ app/models/file.js
@@ -1,6 +1,7 @@
 import Model, { attr } from '@ember-data/model';
 export default class FileModel extends Model {
   @attr('string') name;
+  @attr('string') description;
   @attr('string') format;
   @attr('number') size;
   @attr('string') extension;
