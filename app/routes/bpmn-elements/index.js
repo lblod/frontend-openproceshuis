5ee78b3b3b2bbfd1fbb9eca7d2e29eca90d47bcb
Nordine Bittich <nordine@bittich.be>
2024-03-07 17:11:57 +0100

    mu search

--- app/routes/bpmn-elements/index.js
+++ app/routes/bpmn-elements/index.js
@@ -2,10 +2,9 @@ import Route from '@ember/routing/route';
 import { keepLatestTask } from 'ember-concurrency';
 import { service } from '@ember/service';
 import { InvertedBpmnElementTypes } from '../../utils/bpmn-element-types';
-
 export default class BpmnElementsIndexRoute extends Route {
   @service store;
-
+  @service muSearch;
   queryParams = {
     page: { refreshModel: true },
     sort: { refreshModel: true },
@@ -15,9 +14,81 @@ export default class BpmnElementsIndexRoute extends Route {
 
   async model(params) {
     return {
-      loadBpmnElementsTaskInstance: this.loadbpmnElementsTask.perform(params),
-      loadedBpmnElements: this.loadbpmnElementsTask.lastSuccesful?.value,
+      loadBpmnElementsTaskInstance:
+        this.loadbpmnElementsTaskMuSearch.perform(params),
+      loadedBpmnElements:
+        this.loadbpmnElementsTaskMuSearch.lastSuccesful?.value,
     };
+  }
+
+  @keepLatestTask({ cancelOn: 'deactivate' })
+  *loadbpmnElementsTaskMuSearch(params) {
+    const filter = {};
+    if (params.name) {
+      let filterType = 'phrase_prefix';
+      let name = params.name.trim();
+
+      filter[`:${filterType}:name`] = name;
+    }
+    if (params.type) {
+      const elementType = InvertedBpmnElementTypes[params.type] || undefined;
+      if (elementType)
+        filter[
+          'classification'
+        ] = `https://www.irit.fr/recherches/MELODI/ontologies/BBO#${elementType}`;
+    }
+    let sort = null;
+    if (params.sort) {
+      const isDescending = params.sort.startsWith('-');
+
+      let fieldName = isDescending ? params.sort.substring(1) : params.sort;
+      if (fieldName === 'file') fieldName = 'processes.derivations.name';
+      else if (fieldName === 'name') filter[':has:name'] = 't'; // Filtering with non-existent names, behaves unexpectedly
+
+      sort = `${fieldName}`;
+      if (isDescending) sort = `-${sort}`;
+    }
+
+    return yield this.muSearch.search({
+      index: 'bpmn-elements',
+      page: params.page,
+      size: params.size,
+      sort,
+      filters: filter,
+      dataMapping: (data) => {
+        const entry = data.attributes;
+        const obj = {
+          name: entry.name,
+          id: entry.uuid,
+          type: Array.isArray(entry.classification)
+            ? entry.classification
+                .map((c) =>
+                  c.replace(
+                    'https://www.irit.fr/recherches/MELODI/ontologies/BBO#',
+                    ''
+                  )
+                )
+                .join(', ')
+            : entry.classification?.replace(
+                'https://www.irit.fr/recherches/MELODI/ontologies/BBO#',
+                ''
+              ),
+          process: {
+            derivation: {
+              name: entry.processes?.name,
+              created: entry.processes?.created,
+              modified: Array.isArray(entry.processes?.modified)
+                ? entry.processes.modified[0]
+                : entry.processes?.modified,
+              id: entry.processes?.fileId,
+            },
+          },
+        };
+
+        console.log(entry);
+        return obj;
+      },
+    });
   }
 
   @keepLatestTask({ cancelOn: 'deactivate' })
