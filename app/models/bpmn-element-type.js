bdffcf3956f642f50d341b992ebd9312cafd2bea
Martijn Bogaert <martijn.bogaert@sirus.be>
2024-04-18 18:36:04 +0200

    Standardize Dutch concept labels

--- app/models/bpmn-element-type.js
+++ app/models/bpmn-element-type.js
@@ -1,16 +1,11 @@
 import Model, { attr } from '@ember-data/model';
 
 export default class BpmnElementTypeModel extends Model {
-  @attr('language-string-set') label;
+  @attr('language-string') label;
   @attr('language-string') key;
 
-  get labelNl() {
-    if (!this.label?.length) return null;
-    return (
-      this.label.find((label) => label.language === 'nl')?.content ??
-      this.label.find((label) => label.language.startsWith('nl'))?.content ??
-      this.label[0].content
-    );
+  get name() {
+    return this.label?.content;
   }
 
   get queryValue() {
