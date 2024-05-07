890a0fe92addab971ac91ac737c0e44f4e4aa26e
Martijn Bogaert <martijn.bogaert@sirus.be>
2024-05-07 11:30:12 +0200

    Disable diagram zoom scrolling by default

--- app/components/bpmn-viewer.js
+++ app/components/bpmn-viewer.js
@@ -3,16 +3,25 @@ import { action } from '@ember/object';
 import NavigatedViewer from 'bpmn-js/lib/NavigatedViewer';
 
 export default class BpmnViewerComponent extends Component {
+  viewer = null;
+
   @action
   async setupViewer(element) {
     element.tabIndex = 0; // Make element focusable
-
-    const viewer = new NavigatedViewer({ container: element });
+    this.viewer = new NavigatedViewer({ container: element });
 
     const bpmnXml = this.args.bpmnXml;
-    if (bpmnXml) {
-      await viewer.importXML(bpmnXml);
-      viewer.get('canvas').zoom('fit-viewport');
-    }
+    if (!bpmnXml) return;
+
+    await this.viewer.importXML(bpmnXml);
+    this.viewer.get('canvas').zoom('fit-viewport');
+    this.enableZoomScroll(false);
+  }
+
+  enableZoomScroll(value) {
+    if (!this.viewer) return;
+
+    const zoomScroll = this.viewer.get('zoomScroll');
+    zoomScroll.toggle(value);
   }
 }
