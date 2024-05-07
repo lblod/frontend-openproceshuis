56a13da0cecf306df3696ea7d62a09669718e717
Martijn Bogaert <36764916+MartijnBogaert@users.noreply.github.com>
2024-05-07 12:15:51 +0200

    Disable diagram scrolling by default (#22)
    * Make bpmn viewer focusable
    
    * Disable diagram zoom scrolling by default
    
    * Enable diagram zoom scrolling on focus
    
    * Clean up code

--- app/components/bpmn-viewer.js
+++ app/components/bpmn-viewer.js
@@ -3,14 +3,38 @@ import { action } from '@ember/object';
 import NavigatedViewer from 'bpmn-js/lib/NavigatedViewer';
 
 export default class BpmnViewerComponent extends Component {
+  viewer = null;
+
   @action
   async setupViewer(element) {
-    const viewer = new NavigatedViewer({ container: element });
+    element.tabIndex = 0; // Make element focusable
+    this.viewer = new NavigatedViewer({ container: element });
 
     const bpmnXml = this.args.bpmnXml;
-    if (bpmnXml) {
-      await viewer.importXML(bpmnXml);
-      viewer.get('canvas').zoom('fit-viewport');
-    }
+    if (!bpmnXml) return;
+
+    await this.viewer.importXML(bpmnXml);
+    this.viewer.get('canvas').zoom('fit-viewport');
+    this.disableZoomScroll();
+
+    element.addEventListener('focus', () => {
+      this.enableZoomScroll();
+    });
+
+    element.addEventListener('blur', () => {
+      this.disableZoomScroll();
+    });
+  }
+
+  enableZoomScroll() {
+    if (!this.viewer) return;
+    const zoomScroll = this.viewer.get('zoomScroll');
+    zoomScroll.toggle(true);
+  }
+
+  disableZoomScroll() {
+    if (!this.viewer) return;
+    const zoomScroll = this.viewer.get('zoomScroll');
+    zoomScroll.toggle(false);
   }
 }
