c8ebc450763af32c8fd69ed7021c235834a3836a
Nordine Bittich <nordine@bittich.be>
2024-05-07 16:18:01 +0200

    correct role

--- app/services/current-session.js
+++ app/services/current-session.js
@@ -1,7 +1,7 @@
 import Service, { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
-const EDITOR_ROLES = ['LoketLB-ContactOrganisatiegegevensGebruiker'];
+const EDITOR_ROLES = ['LoketLB-OpenProcesHuisGebruiker'];
 
 export default class CurrentSessionService extends Service {
   @service session;
