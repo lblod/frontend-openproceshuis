ee6b4a492383822e725aa77a6e2341f61a6ea9f9
Martijn Bogaert <martijn.bogaert@sirus.be>
2024-05-21 09:58:54 +0200

    Remove favorites route

--- app/router.js
+++ app/router.js
@@ -24,7 +24,6 @@ Router.map(function () {
     this.route('bpmn-file', { path: '/:id/' }, function () {
       this.route('index', { path: '' });
     });
-    this.route('favorites', { path: 'favorieten' });
   });
 
   this.route('bpmn-uploads', { path: 'gedeelde-processen' }, function () {
