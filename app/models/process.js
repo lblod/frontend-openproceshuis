037ccd40c6af9118b98e0e2b4d48afe06636945e
Martijn Bogaert <martijn.bogaert@sirus.be>
2024-06-12 11:47:07 +0200

    Add datetimes to process model

--- app/models/process.js
+++ app/models/process.js
@@ -4,6 +4,8 @@ import ENV from 'frontend-openproceshuis/config/environment';
 export default class ProcessModel extends Model {
   @attr('string') title;
   @attr('string') description;
+  @attr('iso-date') created;
+  @attr('iso-date') modified;
   @attr('string') status;
   @belongsTo('group', { inverse: null, async: false }) publisher;
   @hasMany('file', { inverse: 'process', async: false }) files;
