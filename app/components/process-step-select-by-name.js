48c043583543ebbce42eae257990a379a61a5193
Martijn Bogaert <martijn.bogaert@sirus.be>
2024-06-24 12:10:30 +0200

    Only show available proces steps in search

--- app/components/process-step-select-by-name.js
+++ app/components/process-step-select-by-name.js
@@ -1,12 +1,21 @@
 import Component from '@glimmer/component';
 import { service } from '@ember/service';
 import { restartableTask } from 'ember-concurrency';
+import ENV from 'frontend-openproceshuis/config/environment';
 
 export default class ProcessStepSelectByNameComponent extends Component {
   @service store;
 
   @restartableTask *loadProcessStepsTask(searchParams = '') {
-    const query = {};
+    const query = {
+      'filter[:has:bpmn-process]': true,
+      'filter[bpmn-process][:has:bpmn-file]': true,
+      'filter[bpmn-process][bpmn-file][:not:status]':
+        ENV.resourceStates.archived,
+      'filter[bpmn-process][bpmn-file][:has:processes]': true,
+      'filter[bpmn-process][bpmn-file][processes][:not:status]':
+        ENV.resourceStates.archived,
+    };
 
     if (searchParams.trim() !== '') {
       query['filter[name]'] = searchParams;
