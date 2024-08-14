d9ad57dbdf92951eef25e5d42273105e2478459b
Martijn Bogaert <martijn.bogaert@sirus.be>
2024-08-14 09:53:26 +0200

    Add validator for process email

--- app/models/process.js
+++ app/models/process.js
@@ -1,7 +1,10 @@
 import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
 import { modelValidator } from 'ember-model-validator';
 import ENV from 'frontend-openproceshuis/config/environment';
-import { isOnlyWhitespace } from 'frontend-openproceshuis/utils/custom-validators';
+import {
+  isOnlyWhitespace,
+  isEmptyOrEmail,
+} from 'frontend-openproceshuis/utils/custom-validators';
 
 @modelValidator
 export default class ProcessModel extends Model {
@@ -26,6 +29,9 @@ export default class ProcessModel extends Model {
         maximum: 3000,
       },
       custom: (_, value) => !isOnlyWhitespace(value),
+    },
+    email: {
+      custom: (_, value) => isEmptyOrEmail(value),
     },
   };
 
--- app/utils/custom-validators.js
+++ app/utils/custom-validators.js
@@ -3,3 +3,14 @@ export function isOnlyWhitespace(str) {
   if (str.trim() !== '') return false; // non-whitespace characters present
   return true;
 }
+
+export function isEmptyOrEmail(str) {
+  if (!str) return true; // undefined, null or ''
+  if (
+    String(str).match(
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/ // email format
+    ) !== null
+  )
+    return true;
+  return false;
+}
