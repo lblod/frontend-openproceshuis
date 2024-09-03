7408c1ebf8b52883d3e0600fd4d9b829f4091334
Martijn Bogaert <martijn.bogaert@sirus.be>
2024-09-03 14:18:21 +0200

    Refactor process route (pt 2)

--- app/routes/processes/process/index.js
+++ app/routes/processes/process/index.js
@@ -1,17 +1,42 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
+import { keepLatestTask } from 'ember-concurrency';
+import ENV from 'frontend-openproceshuis/config/environment';
 
 export default class ProcessesProcessIndexRoute extends Route {
+  @service store;
+  @service plausible;
+
   async model() {
     const { id: processId } = this.paramsFor('processes.process');
 
-    const { loadProcessTaskInstance, loadedProcess } =
-      this.modelFor('processes.process');
-
     return {
       processId,
-      loadProcessTaskInstance,
-      loadedProcess,
+      loadProcessTaskInstance: this.loadProcessTask.perform(),
+      loadedProcess: this.loadProcessTask.lastSuccesful?.value,
     };
+  }
+
+  @keepLatestTask
+  *loadProcessTask() {
+    const { id: processId } = this.paramsFor('processes.process');
+    const query = {
+      reload: true,
+      include:
+        'files,publisher,publisher.primary-site,publisher.primary-site.contacts',
+      'filter[files][:not:status]': ENV.resourceStates.archived,
+    };
+
+    const process = yield this.store.findRecord('process', processId, query);
+
+    this.plausible.trackEvent('Raadpleeg proces', {
+      'Proces-ID': process?.id,
+      Procesnaam: process?.title,
+      'Bestuur-ID': process?.publisher?.id,
+      Bestuursnaam: process?.publisher?.name,
+    });
+
+    return process;
   }
 
   setupController(controller) {
