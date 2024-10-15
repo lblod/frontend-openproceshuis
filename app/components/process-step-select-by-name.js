fd0ffb07a2cddf6ddf55007aedd4547cd146907d
Martijn Bogaert <36764916+MartijnBogaert@users.noreply.github.com>
2024-10-15 13:36:53 +0200

    Use mu-search for process steps (#51)
    * Uncomment old function
    
    * Prepare mu search query body
    
    * Perform musearch call
    
    * Use musearch by default for process steps list
    
    * Add mu search to select by name component
    
    * Refactor code
    
    * Use query string for mu search requests
    
    * Add debounce for options searching
    
    * Provide more context when musearch query fails
    
    * Merge branch 'development' into feature/mu-search-process-steps
    
    * fix exception when some data were not indexed for some reasons
    
    * Don't use dsl
    
    * Remove specific error page
    
    * Remove specific error page (pt 2)
    
    * Sanitize status uris

--- app/components/process-step-select-by-name.js
+++ app/components/process-step-select-by-name.js
@@ -1,31 +1,50 @@
 import Component from '@glimmer/component';
 import { service } from '@ember/service';
-import { restartableTask } from 'ember-concurrency';
+import { restartableTask, timeout } from 'ember-concurrency';
 import ENV from 'frontend-openproceshuis/config/environment';
 
 export default class ProcessStepSelectByNameComponent extends Component {
-  @service store;
+  @service muSearch;
 
   @restartableTask
-  *loadProcessStepsTask(searchParams = '') {
-    const query = {
-      'filter[:has:bpmn-process]': true,
-      'filter[bpmn-process][:has:bpmn-file]': true,
-      'filter[bpmn-process][bpmn-file][:not:status]':
-        ENV.resourceStates.archived,
-      'filter[bpmn-process][bpmn-file][:has:processes]': true,
-      'filter[bpmn-process][bpmn-file][processes][:not:status]':
-        ENV.resourceStates.archived,
-    };
+  *loadProcessStepsTask(searchValue = '') {
+    if (!searchValue?.trim()) return;
 
-    if (searchParams.trim() !== '') {
-      query['filter[name]'] = searchParams;
-    }
+    yield timeout(200);
 
-    const result = yield this.store.query('bpmn-element', query);
+    const page = 0;
+    const size = 50;
 
-    if (result) {
-      return [...[searchParams], ...new Set(result.map((r) => r.name))];
+    const filter = {};
+
+    filter[':has:bpmn-process.bpmn-file.processes'] = 't';
+    filter[':query:name'] = `*${searchValue}*`;
+
+    const encodedArchivedUri = encodeURIComponent(
+      ENV.resourceStates.archived.replaceAll('/', '\\/')
+    );
+    filter[
+      ':query:bpmn-process.bpmn-file.status'
+    ] = `NOT (${encodedArchivedUri})`;
+    filter[
+      ':query:bpmn-process.bpmn-file.processes.status'
+    ] = `NOT (${encodedArchivedUri})`;
+
+    const bpmnElements = yield this.muSearch.search({
+      index: 'bpmn-elements',
+      page,
+      size,
+      filters: filter,
+      dataMapping: (data) => {
+        const entry = data.attributes;
+        return {
+          name: entry.name,
+        };
+      },
+    });
+
+    if (bpmnElements) {
+      return [...[searchValue], ...new Set(bpmnElements.map((r) => r.name))];
     }
   }
 }
