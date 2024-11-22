0e1c5c0e1e826d6251b64fc63868a21dea4aea77
Martijn Bogaert <36764916+MartijnBogaert@users.noreply.github.com>
2024-11-22 14:33:28 +0100

    Add admin functionality (#56)

--- app/components/app-header.js
+++ app/components/app-header.js
@@ -4,4 +4,37 @@ import { service } from '@ember/service';
 export default class AppHeaderComponent extends Component {
   @service currentSession;
   @service session;
+  @service impersonation;
+
+  get userInfo() {
+    let user;
+    let group;
+
+    if (this.impersonation.isImpersonating) {
+      user = this.impersonation.originalAccount.user;
+      group = this.impersonation.originalGroup;
+    } else {
+      user = this.currentSession.user;
+      group = this.currentSession.group;
+    }
+
+    if (!user) {
+      return '';
+    }
+
+    let userInfo = user.fullName;
+    let groupInfo = '';
+
+    if (group?.name) {
+      groupInfo += ` ${group.name}`;
+    }
+
+    groupInfo.trim();
+
+    if (groupInfo.length) {
+      userInfo += ` - ${groupInfo}`;
+    }
+
+    return userInfo;
+  }
 }
