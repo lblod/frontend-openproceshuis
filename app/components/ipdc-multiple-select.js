860b8451b9e3b234f42ac6faa21eb6c640279cb0
Martijn Bogaert <martijn.bogaert@sirus.be>
2025-01-07 12:03:36 +0100

    Use lpdc code or url

--- app/components/ipdc-multiple-select.js
+++ app/components/ipdc-multiple-select.js
@@ -11,6 +11,9 @@ export default class OrganizationTypeMultipleSelectComponent extends Component {
 
     yield timeout(500);
 
+    const input = this.extractNumberOrId(searchParams);
+    console.log(input);
+
     const ipdc1 = this.store.createRecord('ipdc-instance');
     ipdc1.name = 'ipdc1';
     ipdc1.productNumber = '1';
@@ -24,5 +27,24 @@ export default class OrganizationTypeMultipleSelectComponent extends Component {
     ipdc3.productNumber = '3';
 
     return [ipdc1, ipdc2, ipdc3];
+  }
+
+  extractNumberOrId(input) {
+    input = input.trim();
+
+    try {
+      const url = new URL(input);
+      input = url.pathname.split('/').pop();
+    } catch {
+      /* empty */
+    }
+
+    const numberPattern = /^\d+$/;
+    const uuidPattern =
+      /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
+
+    if (numberPattern.test(input) || uuidPattern.test(input)) return input;
+
+    return null;
   }
 }
