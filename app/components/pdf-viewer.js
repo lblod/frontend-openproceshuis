06980ff9744d8312e1ef96484bc30189986a5180
Martijn Bogaert <martijn.bogaert@sirus.be>
2025-03-04 16:06:23 +0100

    Introduce custom pdf viewer

--- app/components/pdf-viewer.js
+++ app/components/pdf-viewer.js
@@ -1,30 +1,39 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { restartableTask } from 'ember-concurrency';
+import pdfjsLib from 'pdfjs-dist/build/pdf';
 import { generateVisioConversionUrl } from 'frontend-openproceshuis/utils/file-download-url';
 
 export default class PdfViewerComponent extends Component {
-  @tracked pdfBlobUrl = null;
+  constructor() {
+    super(...arguments);
+    pdfjsLib.GlobalWorkerOptions.workerSrc =
+      '//cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js';
+  }
 
   @action
-  async setupViewer() {
+  async setupViewer(element) {
+    element.tabIndex = 0; // Make element focusable
+
     const visioFileId = this.args.diagram?.isVisioFile
       ? this.args.diagram.id
       : undefined;
     if (!visioFileId) return;
 
-    const blob = await this.downloadVisioAsPdf.perform(visioFileId);
-    if (!blob) return;
-
-    this.pdfBlobUrl = URL.createObjectURL(blob);
-  }
-
-  @restartableTask
-  *downloadVisioAsPdf(visioFileId) {
     const url = generateVisioConversionUrl(visioFileId);
-    const response = yield fetch(url);
-    if (!response.ok) throw Error(response.status);
-    return yield response.blob();
+
+    const loadingTask = pdfjsLib.getDocument(url);
+    const pdf = await loadingTask.promise;
+
+    const page = await pdf.getPage(1);
+
+    const scale = 1.5;
+    const viewport = page.getViewport({ scale });
+
+    element.width = viewport.width;
+    element.height = viewport.height;
+    const context = element.getContext('2d');
+
+    const renderContext = { canvasContext: context, viewport };
+    await page.render(renderContext).promise;
   }
 }
