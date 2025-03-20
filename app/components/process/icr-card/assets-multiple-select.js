724a0c18fdc4dc6f49e6b6f919b5f1754a6095f6
Martijn Bogaert <martijn.bogaert@sirus.be>
2025-03-20 14:53:57 +0100

    Fetch information assets from backend

--- app/components/process/icr-card/assets-multiple-select.js
+++ app/components/process/icr-card/assets-multiple-select.js
@@ -1,32 +1,41 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { restartableTask, timeout } from 'ember-concurrency';
+import ENV from 'frontend-openproceshuis/config/environment';
 
 export default class ProcessIcrCardAssetsMultipleSelectComponent extends Component {
   @service store;
 
   @restartableTask
   *loadInformationAssetsTask(searchParams = '') {
-    if (!searchParams) return;
+    const searchLabel = searchParams.trim();
+    if (!searchLabel) return;
 
     yield timeout(500);
 
-    return this.assets.filter((asset) =>
-      asset.label.toLowerCase().includes(searchParams.trim().toLowerCase())
-    );
-  }
+    const query = {
+      filter: {
+        label: searchLabel,
+        ':exact:scheme': ENV.conceptSchemes.informationAssets,
+      },
+      sort: ':no-case:label',
+    };
 
-  // Temp
-  assets = [
-    { id: '5e89f94e-0569-4fd4-873c-dad047f14175', label: 'abonnement' },
-    { id: '076f0d37-52ba-4844-88fd-93a420d4b812', label: 'abonnementsplaats' },
-    {
-      id: 'ae79a6b7-ae35-4b82-91d6-55638711c3cf',
-      label: 'ambulante activiteit',
-    },
-    {
-      id: '99c21667-f98c-4b60-a721-faef1a0ca67a',
-      label: 'lang label om te testen',
-    },
-  ];
+    let assets;
+    try {
+      const result = yield this.store.query('information-asset', query) ?? [];
+
+      if (
+        result.some((r) => r.label.toLowerCase() === searchLabel.toLowerCase())
+      ) {
+        assets = result;
+      } else {
+        assets = [{ label: searchLabel, isDraft: true }, ...result];
+      }
+    } catch {
+      assets = [];
+    }
+
+    return assets;
+  }
 }
