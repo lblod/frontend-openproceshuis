b6d70c37d409511b62886b5d52a39efe125ec85e
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-08-26 08:38:28 +0200

    feat: when passing on a process id that cannot be found it will show the error page
    Refs: oph-86

--- app/routes/processes/process.js
+++ app/routes/processes/process.js
@@ -1,9 +1,12 @@
 import Route from '@ember/routing/route';
+
 import { service } from '@ember/service';
+
 import ENV from 'frontend-openproceshuis/config/environment';
 
 export default class ProcessesProcessRoute extends Route {
   @service store;
+  @service router;
   @service session;
 
   beforeModel(transition) {
@@ -13,12 +16,28 @@ export default class ProcessesProcessRoute extends Route {
   }
 
   async model(params) {
-    const query = {
-      reload: true,
-      include:
-        'process-statistics,files,publisher,publisher.primary-site,publisher.primary-site.contacts,publisher.classification,ipdc-products,information-assets,linked-concept,linked-concept.process-groups.process-domains,linked-concept.process-groups.process-domains.process-categories',
-      'filter[files][:not:status]': ENV.resourceStates.archived,
-    };
-    return await this.store.findRecord('process', params.id, query);
+    try {
+      const process = await this.store.findRecord('process', params.id, {
+        'filter[files][:not:status]': ENV.resourceStates.archived,
+        include: [
+          'process-statistics',
+          'files',
+          'publisher',
+          'publisher.primary-site',
+          'publisher.primary-site.contacts',
+          'publisher.classification',
+          'ipdc-products',
+          'information-assets',
+          'linked-concept',
+          'linked-concept.process-groups.process-domains',
+          'linked-concept.process-groups.process-domains.process-categories',
+        ].join(','),
+        reload: true,
+      });
+
+      return process;
+    } catch (error) {
+      throw new Error(`Er werd geen process met id "${params.id}" gevonden.`);
+    }
   }
 }
