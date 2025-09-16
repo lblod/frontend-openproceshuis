b592fc5820fade5378861e6bcb7fbd48be10a884
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-09-16 15:19:05 +0200

    feat: can add a link to a process
    Refs: oph-671

--- app/components/process/relevant-links.js
+++ app/components/process/relevant-links.js
@@ -2,12 +2,19 @@ import Component from '@glimmer/component';
 
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { service } from '@ember/service';
 
 import { isEmptyOrUrl } from '../../utils/custom-validators';
 
 export default class ProcessRelevantLinks extends Component {
+  @service store;
+
   @tracked isAddModalOpen = false;
   @tracked linkValue;
+
+  get relevantLinks() {
+    return this.args.process?.links ?? [];
+  }
 
   get inputIsValid() {
     return isEmptyOrUrl(this.linkValue);
@@ -20,18 +27,30 @@ export default class ProcessRelevantLinks extends Component {
     return this.inputIsValid;
   }
 
-  get relevantLinks() {
-    return [];
-  }
-
   @action
   updateLinkValue(event) {
     this.linkValue = event.target?.value;
   }
 
   @action
-  addLink() {
+  async addLink() {
     const cleanLink = this.linkValue.trim();
-    alert('Add link TODO => ' + cleanLink);
+    const link = this.store.createRecord('link', {
+      label: null,
+      href: this.linkValue,
+    });
+    const links = await this.args.process.links;
+    links.push(cleanLink);
+
+    if (links.find((l) => l.href === link.href)) {
+      this.toaster.error('Deze link heeft u al toegevoegd', undefined, {
+        timeOut: 5000,
+      });
+      link.deleteRecord();
+      return;
+    }
+
+    await link.save();
+    await this.args.process.save();
   }
 }
