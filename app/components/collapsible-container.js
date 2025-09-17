9f694f19b7a8129e8b6da18ae53fdca46ce96916
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-09-17 16:17:10 +0200

    feat: create collapsible-container components
    Kudos to @Andreo
    
    Refs: oph-665

---
+++ app/components/collapsible-container.js
@@ -0,0 +1,13 @@
+import Component from '@glimmer/component';
+
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class CollapsibleContainer extends Component {
+  @tracked isCollapsed = false;
+
+  @action
+  toggleCollapse() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+}
