8b97328a8e1332e3d3459ffb134c171ccacdd16f
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-09-17 11:50:32 +0200

    feat: user can find ipdc products by typing
    The "zoekterm" param is returning weird results AFAIK
    
    Refs: oph-691

--- app/components/process/details-card/ipdc-multiple-select.js
+++ app/components/process/details-card/ipdc-multiple-select.js
@@ -11,29 +11,52 @@ export default class ProcessDetailsCardIpdcMultipleSelectComponent extends Compo
 
     yield timeout(500);
 
+    let results = [];
     const productNumberOrId = this.extractNumberOrId(searchParams);
-    if (!productNumberOrId) return;
+    if (productNumberOrId) {
+      const response = yield fetch(`/ipdc/doc/product/${productNumberOrId}`);
+      if (!response.ok) return [];
 
-    const response = yield fetch(`/ipdc/doc/product/${productNumberOrId}`);
-    if (!response.ok) return;
-    const productJson = yield response.json();
+      const jsonResponse = yield response.json();
+      this.throwErrorOnUnsupportedResponseType(jsonResponse);
 
-    let type;
-    if (productJson['@type'] === 'Instantie') type = 'ipdc-instance';
-    else if (productJson['@type'] === 'Concept') type = 'ipdc-concept';
-    else return;
+      results = [jsonResponse];
+    } else {
+      const response = yield fetch(
+        `/ipdc/doc/product?zoekterm=${searchParams}`,
+      );
+      if (!response.ok) return [];
 
-    const draftIpdcProduct = {
-      name: Object.entries(productJson.naam).map(([language, content]) => ({
-        content,
-        language,
-      })),
-      productNumber: productJson.productnummer,
-      type,
-      isDraft: true,
+      const jsonResponse = yield response.json();
+      this.throwErrorOnUnsupportedResponseType(jsonResponse);
+
+      results = jsonResponse.hydraMember ?? []; // Default imit is 25
+    }
+
+    const typeMapping = {
+      ['Instantie']: 'ipdc-instance',
+      ['Concept']: 'ipdc-concept',
     };
+    return results.map((product) => {
+      return {
+        name: Object.entries(product.naam).map(([language, content]) => ({
+          content,
+          language,
+        })),
+        productNumber: product.productnummer,
+        type: typeMapping[product['@type']],
+        isDraft: true,
+      };
+    });
+  }
 
-    return [draftIpdcProduct];
+  throwErrorOnUnsupportedResponseType(jsonResponse) {
+    const type = jsonResponse['@type'];
+    if (['Collection', 'Instantie', 'Concept'].includes(type)) {
+      return;
+    }
+
+    throw new Error('Unsupported type in IPDC response: ' + type);
   }
 
   extractNumberOrId(input) {
