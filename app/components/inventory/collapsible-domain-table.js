e320898292d3cce11282507fdb66c434f3e979a9
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-09-18 10:23:02 +0200

    feat: only fetch the groups when they are opened + add loaders
    - The calls take some time if they are not in cache, added a loader so the user knows that it is fetching data
    - Only load the groups as this is a heavy query apparently when the table is opened
    
    Refs: oph-665

--- app/components/inventory/collapsible-domain-table.js
+++ app/components/inventory/collapsible-domain-table.js
@@ -10,22 +10,32 @@ export default class InventoryCollapsibleDomainTable extends Component {
   @service store;
 
   @tracked groups = A([]);
+  @tracked isLoading = false;
   @tracked isCollapsed = false;
 
   constructor() {
     super(...arguments);
     this.isCollapsed = !!this.args.isCollapsedOnLoad;
-    this.store
-      .query('process-group', {
-        'filter[process-domains][id]': this.args.domain.id,
-        sort: 'label',
-        page: { size: 100 },
-      })
-      .then((groups) => this.groups.pushObjects(groups));
+    if (!this.isCollapsed) {
+      this.loadGroups();
+    }
   }
 
   @action
-  toggleCollapse() {
+  async toggleCollapse() {
     this.isCollapsed = !this.isCollapsed;
+    if (!this.isCollapsed) {
+      await this.loadGroups();
+    }
+  }
+
+  async loadGroups() {
+    this.isLoading = true;
+    this.groups = await this.store.query('process-group', {
+      'filter[process-domains][id]': this.args.domain.id,
+      sort: 'label',
+      page: { size: 100 },
+    });
+    this.isLoading = false;
   }
 }
--- app/components/inventory/table-view-domains.js
+++ app/components/inventory/table-view-domains.js
@@ -9,15 +9,20 @@ export default class InventoryTableViewDomains extends Component {
   @service store;
 
   @tracked domains = A([]);
+  @tracked isLoading = false;
 
   constructor() {
     super(...arguments);
+    this.isLoading = true;
     this.store
       .query('process-domain', {
         'filter[process-categories][id]': this.args.category.id,
         sort: 'label',
         page: { size: 100 },
       })
-      .then((domains) => this.domains.pushObjects(domains));
+      .then((domains) => {
+        this.isLoading = false;
+        this.domains.pushObjects(domains);
+      });
   }
 }
