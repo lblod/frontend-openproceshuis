2312061e923447f33090273ff329505e4ff57039
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-09-18 09:22:57 +0200

    feat: show the groups of the domain in a collapsible table
    Refs: oph-665

---
+++ app/components/inventory/collapsible-domain-table.js
@@ -0,0 +1,30 @@
+import Component from '@glimmer/component';
+
+import { A } from '@ember/array';
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+
+import { tracked } from '@glimmer/tracking';
+
+export default class InventoryCollapsibleDomainTable extends Component {
+  @service store;
+
+  @tracked groups = A([]);
+  @tracked isCollapsed = false;
+
+  constructor() {
+    super(...arguments);
+    this.store
+      .query('process-group', {
+        'filter[process-domains][id]': this.args.domain.id,
+        sort: 'label',
+        page: { size: 100 },
+      })
+      .then((groups) => this.groups.pushObjects(groups));
+  }
+
+  @action
+  toggleCollapse() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+}
