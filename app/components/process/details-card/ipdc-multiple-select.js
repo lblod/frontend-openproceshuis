ce35a4e65e3fa256d7e2ecd5121c19ac83de9486
Jonas Van Hoof <36266973+JonasVanHoof@users.noreply.github.com>
2025-09-25 12:13:05 +0200

    feat: user can find ipdc products by typing (#133)
    * feat: user can find ipdc products by typing
    
    The "zoekterm" param is returning weird results AFAIK
    
    Refs: oph-691
    
    * feat: add extra explicit queryParam to only search on non archived
    
    This param makes sure we only show result for products that have not been archived yet
    
    Refs: oph-691
    
    * hack: only show the results where the title includes the searchparam for ipdc
    
    The api does not support to only search for the title content. This means that we have to do this frontend hack
    
    Refs: oph-691

--- app/components/process/details-card/ipdc-multiple-select.js
+++ app/components/process/details-card/ipdc-multiple-select.js
@@ -11,29 +11,67 @@ export default class ProcessDetailsCardIpdcMultipleSelectComponent extends Compo
 
     yield timeout(500);
 
+    let results = [];
     const productNumberOrId = this.extractNumberOrId(searchParams);
-    if (!productNumberOrId) return;
+    if (productNumberOrId) {
+      const response = yield fetch(`/ipdc/doc/product/${productNumberOrId}`);
+      if (!response.ok) return [];
 
-    const response = yield fetch(`/ipdc/doc/product/${productNumberOrId}`);
-    if (!response.ok) return;
-    const productJson = yield response.json();
+      const jsonResponse = yield response.json();
+      this.throwErrorOnUnsupportedResponseType(jsonResponse);
 
-    let type;
-    if (productJson['@type'] === 'Instantie') type = 'ipdc-instance';
-    else if (productJson['@type'] === 'Concept') type = 'ipdc-concept';
-    else return;
+      results = [jsonResponse];
+    } else {
+      const response = yield fetch(
+        `/ipdc/doc/product?gearchiveerd=false&zoekterm=${searchParams}`,
+      );
+      if (!response.ok) return [];
 
-    const draftIpdcProduct = {
-      name: Object.entries(productJson.naam).map(([language, content]) => ({
-        content,
-        language,
-      })),
-      productNumber: productJson.productnummer,
-      type,
-      isDraft: true,
+      const jsonResponse = yield response.json();
+      this.throwErrorOnUnsupportedResponseType(jsonResponse);
+
+      results = jsonResponse.hydraMember ?? []; // Default imit is 25
+    }
+
+    const typeMapping = {
+      ['Instantie']: 'ipdc-instance',
+      ['Concept']: 'ipdc-concept',
     };
+    return results
+      .map((product) => {
+        const name = Object.entries(product.naam).map(
+          ([language, content]) => ({
+            content,
+            language,
+          }),
+        );
+        // TODO: Update the queryParams when the api supports only searching in title content
+        const namesJoin = name.map((n) => n.content).join(' ');
+        if (
+          !productNumberOrId &&
+          searchParams &&
+          !namesJoin.includes(searchParams)
+        ) {
+          return null;
+        }
 
-    return [draftIpdcProduct];
+        return {
+          name,
+          productNumber: product.productnummer,
+          type: typeMapping[product['@type']],
+          isDraft: true,
+        };
+      })
+      .filter((isProduct) => isProduct);
+  }
+
+  throwErrorOnUnsupportedResponseType(jsonResponse) {
+    const type = jsonResponse['@type'];
+    if (['Collection', 'Instantie', 'Concept'].includes(type)) {
+      return;
+    }
+
+    throw new Error('Unsupported type in IPDC response: ' + type);
   }
 
   extractNumberOrId(input) {
