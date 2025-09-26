70e8700cd9bcada6be72a081d4f211e4de304309
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-09-26 12:10:45 +0200

    feat: create label with status pill component
    Can add a label or a statusUri of both
    
    Refs: oph-716

---
+++ app/components/inventory/label-with-status-pill.js
@@ -0,0 +1,17 @@
+import Component from '@glimmer/component';
+
+import ENV from 'frontend-openproceshuis/config/environment';
+
+export default class InventoryLabelWithStatusPill extends Component {
+  get status() {
+    const statusUriMap = [
+      {
+        uri: ENV.resourceStates.archived,
+        label: 'Gearchiveerd',
+        skin: 'warning',
+      },
+    ];
+
+    return statusUriMap.find((s) => s.uri === this.args.statusUri);
+  }
+}
