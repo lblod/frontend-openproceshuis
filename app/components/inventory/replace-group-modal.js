05bbfa28a1e22e960474dad05ffeece5f2e3d7af
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-10-09 14:08:52 +0200

    feat: create replaceGroupModal component
    - replace with modal in the admin admin page
    - Fetch the category options and show them in the dropdown
    
    Refs: oph-722

---
+++ app/components/inventory/replace-group-modal.js
@@ -0,0 +1,31 @@
+import Component from '@glimmer/component';
+
+import { A } from '@ember/array';
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+
+import { restartableTask } from 'ember-concurrency';
+import { tracked } from '@glimmer/tracking';
+
+import ENV from 'frontend-openproceshuis/config/environment';
+
+export default class InventoryReplaceGroupModal extends Component {
+  @service store;
+
+  @tracked categoryOptions = A([]);
+  @tracked newCategory;
+
+  loadCategories = restartableTask(async () => {
+    const categories = await this.store.query('process-category', {
+      'filter[:exact:scheme]': ENV.conceptSchemes.processCategories,
+    });
+
+    this.categoryOptions.clear();
+    this.categoryOptions.pushObjects(categories);
+  });
+
+  @action
+  setNewProcessCategory(category) {
+    console.log('new category', category);
+  }
+}
