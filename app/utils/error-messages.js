4e2a6d8e97a25d361aa491010d94a49d4239a0aa
Jonas Van Hoof <36266973+JonasVanHoof@users.noreply.github.com>
2025-12-16 10:08:19 +0100

    OPH-822, 824 | rework displayed error messages (#159)
    * refactor: update the textual content of the error messages shown in the frontend
    
    Refs: oph-822
    
    * feat: create contact us links in the toaster message
    
    Refs: oph-824
    
    * refactor: add openingline + format body
    
    Refs: oph-824
    
    * refactor: remove opening line
    
    Its more like a disclaimer info that needs to stay there
    
    Refs: oph-822
    
    * refactor: also update text and links for the bpmn and visio frontend errors
    
    Refs: oph-822
    
    * refactor: make common values so we can make less irregularities
    
    Refs: oph-822
    
    * refactor: instead of deel het hier => deel het hieronder
    
    Refs: oph-822
    
    * fix: api error not shown when uploading bpmn as wrong fetch is used
    
    Refs: oph-822
    
    * Handle fetch call by api service
    
    ---------
    
    Co-authored-by: Martijn Bogaert <martijn.bogaert@sirus.be>

--- app/utils/error-messages.js
+++ app/utils/error-messages.js
@@ -1,54 +1,196 @@
 import { htmlSafe } from '@ember/template';
 
-const getVisioDownloadErrorMessage = () => {
+const getContactMailToHtml = ({ visualText, mailSubject, mailBody }) => {
+  const clickableText = visualText ?? 'Contacteer ons.';
   const mailto =
     'mailto:loketlokaalbestuur@vlaanderen.be' +
-    `?subject=${encodeURIComponent('Visio kan niet downloaden als BPMN')}` +
-    `?body=${encodeURIComponent(`\n\n${window.location.href}\n`)}`;
-  const linkHtml = `<a href="${mailto}">Stuur ons een mailtje</a>`;
-  return htmlSafe(
-    `Dit visio-bestand kan niet worden gedownload als BPMN. ${linkHtml} met het proces waarover het gaat en een korte beschrijving van wat niet lukt. Dan kunnen we nagaan wat fout gaat en Open Proces Huis verder verbeteren.`,
-  );
+    `?subject=${encodeURIComponent(mailSubject)}` +
+    `&body=${encodeURIComponent(mailBody)}`;
+  return `<a href="${mailto}" target="_blank" >${clickableText}</a>`;
 };
 
+const getTimestampForMailBody = () => new Date().toLocaleString();
+const getCommonMailSubject = (errorCode) => `OPH - fout ${errorCode} oplossen`;
+
 export const ERROR_CODES = {
-  // FRONTEND
-  'oph.fileDeletionError': 'Dit bestand kon niet verwijderd worden.',
-  'oph.processDeletionError': 'Dit proces kon niet verwijderd worden.',
-  'oph.updateModelFailed': 'Dit proces kon niet worden bijgewerkt.',
-  'oph.icrDataUpdateFailed':
-    'Informatieclassificatie data kon niet worden bijgewerkt.',
-  'oph.visioLatestDiagramDownloadFailed': getVisioDownloadErrorMessage(),
-  'oph.downloadLatestDiagramFailed': 'Bestand kon niet worden opgehaald',
+  'oph.fileDeletionError': htmlSafe(
+    `Het verwijderen van het bestand is mislukt. Herlaad de pagina en probeer opnieuw. Blijft het probleem? ${getContactMailToHtml(
+      {
+        mailSubject: getCommonMailSubject('oph.fileDeletionError'),
+        mailBody: `Deze email wordt gestuurd om de fout "oph.fileDeletionError" op ${getTimestampForMailBody()} op te lossen.\nHeb je meer relevante details die ons het probleem zouden kunnen helpen oplossen? Deel ze hieronder.`,
+      },
+    )}`,
+  ),
+  'oph.processDeletionError': htmlSafe(
+    `Het verwijderen van het proces is mislukt. Herlaad de pagina en probeer opnieuw. Blijft het probleem? ${getContactMailToHtml(
+      {
+        mailSubject: getCommonMailSubject('oph.processDeletionError'),
+        mailBody: `Deze email wordt gestuurd om de fout "oph.processDeletionError" op ${getTimestampForMailBody()} op te lossen.\nHeb je meer relevante details die ons het probleem zouden kunnen helpen oplossen? Deel ze hieronder.`,
+      },
+    )}`,
+  ),
+  'oph.updateModelFailed': htmlSafe(
+    `Het bijwerken van het proces is mislukt. Herlaad de pagina en probeer opnieuw. Blijft het probleem? ${getContactMailToHtml(
+      {
+        mailSubject: getCommonMailSubject('oph.updateModelFailed'),
+        mailBody: `Deze email wordt gestuurd om de fout "oph.updateModelFailed" op ${getTimestampForMailBody()} op te lossen.\nHeb je meer relevante details, zoals bijvoorbeeld de namen van de velden die je wou bijwerken, die ons het probleem zouden kunnen helpen oplossen? Deel ze hieronder.`,
+      },
+    )}`,
+  ),
+  'oph.icrDataUpdateFailed': htmlSafe(
+    `Het bijwerken van de informatieclassificatie data is mislukt. Herlaad de pagina en probeer opnieuw. Blijft het probleem? ${getContactMailToHtml(
+      {
+        mailSubject: getCommonMailSubject('oph.icrDataUpdateFailed'),
+        mailBody: `Deze email wordt gestuurd om de fout "oph.icrDataUpdateFailed" op ${getTimestampForMailBody()} op te lossen.\nHeb je meer relevante details, zoals bijvoorbeeld de namen van de velden die je wou bijwerken, die ons het probleem zouden kunnen helpen oplossen? Deel ze hieronder.`,
+      },
+    )}`,
+  ),
+  'oph.visioLatestDiagramDownloadFailed': htmlSafe(
+    `Het visio-bestand kon niet worden gedownload als BPMN. Herlaad de pagina en probeer opnieuw. Blijft het probleem? ${getContactMailToHtml(
+      {
+        mailSubject: getCommonMailSubject(
+          'oph.visioLatestDiagramDownloadFailed',
+        ),
+        mailBody: `Deze email wordt gestuurd om de fout "oph.visioLatestDiagramDownloadFailed" op ${getTimestampForMailBody()} op te lossen.\nHeb je meer relevante details die ons het probleem zouden kunnen helpen oplossen? Deel ze hieronder.`,
+      },
+    )}`,
+  ),
+  'oph.downloadLatestDiagramFailed': htmlSafe(
+    `Het diagram kon niet worden gedownload. Herlaad de pagina en probeer opnieuw. Blijft het probleem? ${getContactMailToHtml(
+      {
+        mailSubject: getCommonMailSubject('oph.downloadLatestDiagramFailed'),
+        mailBody: `Deze email wordt gestuurd om de fout "oph.downloadLatestDiagramFailed" op ${getTimestampForMailBody()} op te lossen.\nHeb je meer relevante details die ons het probleem zouden kunnen helpen oplossen? Deel ze hieronder.`,
+      },
+    )}`,
+  ),
   'oph.addProcessFailed': 'Dit proces kon niet worden toegevoegd',
-  // BPMN
-  'bpmn.sessionIdNotFound': 'Session ID header werd niet gevonden.',
-  'bpmn.groupUriNotFound': 'Gebruiker maakt geen deel uit van een organisatie.',
-  'bpmn.emptyVirtualFileId':
-    'Bestand id ontbrak tijdens het uploaden van het bpmn bestand.',
-  'bpmn.virtualFileIdNotFound': 'Bestand id werd niet gevonden in onze server.',
-  'bpmn.physicalFileIdNotFound':
-    'Kon de fysieke bestandslocatie niet achterhalen',
-  'bpmn.emptyContent':
-    'Ongeldige inhoud: Het meegeleverde bestand bevat geen inhoud.',
-  'bpmn.invalidContent':
-    'Ongeldige inhoud: Het meegeleverde bestand heeft geen geldige inhoud.',
-  'bpmn.errorDuringJobExecution': 'Error tijdens het uitvoeren van job',
+  'bpmn.sessionIdNotFound': htmlSafe(
+    `De session ID header werd niet gevonden. Herlaad de pagina en probeer opnieuw. Blijft het probleem? ${getContactMailToHtml(
+      {
+        mailSubject: getCommonMailSubject('bpmn.sessionIdNotFound'),
+        mailBody: `Deze email wordt gestuurd om de fout "bpmn.sessionIdNotFound" op ${getTimestampForMailBody()} op te lossen.\nHeb je meer relevante details die ons het probleem zouden kunnen helpen oplossen? Deel ze hieronder.`,
+      },
+    )}`,
+  ),
+  'bpmn.groupUriNotFound': htmlSafe(
+    `De actie die je wou uitvoeren is mislukt omdat je niet meer ingelogd bent. Log opnieuw in en probeer nog eens. Blijft het probleem? ${getContactMailToHtml(
+      {
+        mailSubject: getCommonMailSubject('bpmn.groupUriNotFound'),
+        mailBody: `Deze email wordt gestuurd om de fout "bpmn.groupUriNotFound" op ${getTimestampForMailBody()} op te lossen.\nHeb je meer relevante details die ons het probleem zouden kunnen helpen oplossen? Deel ze hieronder.`,
+      },
+    )}`,
+  ),
+  'bpmn.emptyVirtualFileId': htmlSafe(
+    `Het opladen van het bestand is mislukt. Herlaad de pagina en probeer opnieuw. Blijft het probleem? ${getContactMailToHtml(
+      {
+        mailSubject: getCommonMailSubject('bpmn.emptyVirtualFileId'),
+        mailBody: `Deze email wordt gestuurd om de fout "bpmn.emptyVirtualFileId" op ${getTimestampForMailBody()} op te lossen.\nHeb je meer relevante details die ons het probleem zouden kunnen helpen oplossen? Deel ze hieronder.`,
+      },
+    )}`,
+  ),
+  'bpmn.virtualFileIdNotFound': htmlSafe(
+    `De actie die je wou uitvoeren is mislukt omdat we het bestand niet kunnen lokaliseren. ${getContactMailToHtml(
+      {
+        visualText: 'Contacteer ons',
+        mailSubject: getCommonMailSubject('bpmn.virtualFileIdNotFound'),
+        mailBody: `Deze email wordt gestuurd om de fout "bpmn.virtualFileIdNotFound" op ${getTimestampForMailBody()} op te lossen.\nHeb je meer relevante details die ons het probleem zouden kunnen helpen oplossen? Deel ze hieronder.`,
+      },
+    )} om dit probleem op te lossen.`,
+  ),
+  'bpmn.physicalFileIdNotFound': htmlSafe(
+    `De actie die je wou uitvoeren is mislukt omdat we het bestand niet kunnen lokaliseren. ${getContactMailToHtml(
+      {
+        visualText: 'Contacteer ons',
+        mailSubject: getCommonMailSubject('bpmn.physicalFileIdNotFound'),
+        mailBody: `Deze email wordt gestuurd om de fout "bpmn.physicalFileIdNotFound" op ${getTimestampForMailBody()} op te lossen.\nHeb je meer relevante details die ons het probleem zouden kunnen helpen oplossen? Deel ze hieronder.`,
+      },
+    )} om dit probleem op te lossen.`,
+  ),
+  'bpmn.emptyContent': htmlSafe(
+    `Het opladen van het BPMN-bestand is mislukt. ${getContactMailToHtml({
+      visualText: 'Contacteer ons',
+      mailSubject: getCommonMailSubject('bpmn.emptyContent'),
+      mailBody: `Deze email wordt gestuurd om de fout "bpmn.emptyContent" op ${getTimestampForMailBody()} op te lossen.\nVoeg eventueel het bestand toe aan deze email, dan kunnen we je sneller helpen.\nHeb je meer relevante details die ons het probleem zouden kunnen helpen oplossen? Deel ze hieronder.`,
+    })} om dit probleem op te lossen.`,
+  ),
+  'bpmn.invalidContent': htmlSafe(
+    `Het opladen van het BPMN-bestand is mislukt. ${getContactMailToHtml({
+      visualText: 'Contacteer ons',
+      mailSubject: getCommonMailSubject('bpmn.invalidContent'),
+      mailBody: `Deze email wordt gestuurd om de fout "bpmn.invalidContent" op ${getTimestampForMailBody()} op te lossen.\nVoeg eventueel het bestand toe aan deze email, dan kunnen we je sneller helpen.\nHeb je meer relevante details die ons het probleem zouden kunnen helpen oplossen? Deel ze hieronder.`,
+    })} om dit probleem op te lossen.`,
+  ),
+  'bpmn.errorDuringJobExecution': htmlSafe(
+    `Het diagram kon niet worden geüpdatet. Herlaad de pagina en probeer opnieuw. Blijft het probleem? ${getContactMailToHtml(
+      {
+        visualText: 'Contacteer ons',
+        mailSubject: getCommonMailSubject('bpmn.errorDuringJobExecution'),
+        mailBody: `Deze email wordt gestuurd om de fout "bpmn.errorDuringJobExecution" op ${getTimestampForMailBody()} op te lossen.\nVoeg eventueel het bestand toe aan deze email, dan kunnen we je sneller helpen.\nHeb je meer relevante details die ons het probleem zouden kunnen helpen oplossen? Deel ze hieronder.`,
+      },
+    )}`,
+  ),
   'bpmn.fallBackError': 'Onbekende fout tijdens extraheren van processtappen',
-  // VISIO
-  'visio.emptyVirtualFileId':
-    'Bestand id ontbrak tijdens het uploaden van het bpmn bestand.',
-  'visio.virtualFileIdNotFound': 'Bestand-ID werd niet teruggevonden.',
+  'visio.emptyVirtualFileId': htmlSafe(
+    `Het opladen van het bestand is mislukt. Herlaad de pagina en probeer opnieuw. Blijft het probleem? ${getContactMailToHtml(
+      {
+        mailSubject: getCommonMailSubject('visio.emptyVirtualFileId'),
+        mailBody: `Deze email wordt gestuurd om de fout "visio.emptyVirtualFileId" op ${getTimestampForMailBody()} op te lossen.\nHeb je meer relevante details die ons het probleem zouden kunnen helpen oplossen? Deel ze hieronder.`,
+      },
+    )}`,
+  ),
+  'visio.virtualFileIdNotFound': htmlSafe(
+    `De actie die je wou uitvoeren is mislukt omdat we het bestand niet kunnen lokaliseren. ${getContactMailToHtml(
+      {
+        visualText: 'Contacteer ons',
+        mailSubject: getCommonMailSubject('visio.virtualFileIdNotFound'),
+        mailBody: `Deze email wordt gestuurd om de fout "visio.virtualFileIdNotFound" op ${getTimestampForMailBody()} op te lossen.\nHeb je meer relevante details die ons het probleem zouden kunnen helpen oplossen? Deel ze hieronder.`,
+      },
+    )} om dit probleem op te lossen.`,
+  ),
   'visio.vsdxFileTypeExpected':
     'Ongeldig bestandsformaat: .vsdx-extensie verwacht.',
-  'visio.physicalFileIdNotFound':
-    'Kon de fysieke bestandslocatie niet achterhalen.',
+  'visio.physicalFileIdNotFound': htmlSafe(
+    `De actie die je wou uitvoeren is mislukt omdat we het bestand niet kunnen lokaliseren. ${getContactMailToHtml(
+      {
+        visualText: 'Contacteer ons',
+        mailSubject: getCommonMailSubject('visio.physicalFileIdNotFound'),
+        mailBody: `Deze email wordt gestuurd om de fout "visio.physicalFileIdNotFound" op ${getTimestampForMailBody()} op te lossen.\nHeb je meer relevante details die ons het probleem zouden kunnen helpen oplossen? Deel ze hieronder.`,
+      },
+    )} om dit probleem op te lossen.`,
+  ),
   'visio.targetExtensionNotSupported': 'Ongeldige doelformaat-extensie.',
-  'visio.pdfConversionFailed': 'Conversie naar PDF is mislukt.',
-  'visio.pdfConversionFilePathError':
-    'PDF-bestand werd niet gevonden na conversie.',
-  'visio.bpmnConversionFailed': 'Conversie naar BPMN is mislukt.',
-  'visio.fallBackError': 'Onbekende fout tijdens visio conversie',
+  'visio.pdfConversionFailed': htmlSafe(
+    `Het omzetten van het visio-bestand naar PDF is mislukt. Herlaad de pagina en probeer opnieuw. Blijft het probleem? ${getContactMailToHtml(
+      {
+        mailSubject: getCommonMailSubject('visio.pdfConversionFailed'),
+        mailBody: `Deze email wordt gestuurd om de fout "visio.pdfConversionFailed" op ${getTimestampForMailBody()} op te lossen.\nHeb je meer relevante details die ons het probleem zouden kunnen helpen oplossen? Deel ze hieronder.`,
+      },
+    )}`,
+  ),
+  'visio.pdfConversionFilePathError': htmlSafe(
+    `Het omzetten van het visio-bestand naar PDF is mislukt. Herlaad de pagina en probeer opnieuw. Blijft het probleem? ${getContactMailToHtml(
+      {
+        mailSubject: getCommonMailSubject('visio.pdfConversionFilePathError'),
+        mailBody: `Deze email wordt gestuurd om de fout "visio.pdfConversionFilePathError" op ${getTimestampForMailBody()} op te lossen.\nHeb je meer relevante details die ons het probleem zouden kunnen helpen oplossen? Deel ze hieronder.`,
+      },
+    )}`,
+  ),
+  'visio.bpmnConversionFailed': htmlSafe(
+    `Het omzetten van het visio-bestand naar BPMN is mislukt. Herlaad de pagina en probeer opnieuw. Blijft het probleem? ${getContactMailToHtml(
+      {
+        mailSubject: getCommonMailSubject('visio.bpmnConversionFailed'),
+        mailBody: `Deze email wordt gestuurd om de fout "visio.bpmnConversionFailed" op ${getTimestampForMailBody()} op te lossen.\nHeb je meer relevante details die ons het probleem zouden kunnen helpen oplossen? Deel ze hieronder.`,
+      },
+    )}`,
+  ),
+  'visio.fallBackError': htmlSafe(
+    `Het omzetten van het diagram naar een ander formaat is mislukt. Herlaad de pagina en probeer opnieuw. Blijft het probleem? ${getContactMailToHtml(
+      {
+        mailSubject: getCommonMailSubject('visio.fallBackError'),
+        mailBody: `Deze email wordt gestuurd om de fout "visio.fallBackError" op ${getTimestampForMailBody()} op te lossen.\nHeb je meer relevante details die ons het probleem zouden kunnen helpen oplossen? Deel ze hieronder.`,
+      },
+    )}`,
+  ),
 };
 
 export const getMessageForErrorCode = (key) =>
