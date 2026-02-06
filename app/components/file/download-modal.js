042f1ced5f707646247dcd365194dc458ff76734
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2026-02-06 11:22:24 +0100

    feat: create file download component (visually)
    Refs: oph-903

--- app/components/file/download-modal.js
+++ app/components/file/download-modal.js
@@ -1,7 +1,69 @@
 import Component from '@glimmer/component';
 
+import { service } from '@ember/service';
+
 export default class FileDownloadModal extends Component {
-  get something() {
-    return 1;
+  @service toaster;
+
+  get downloadOptions() {
+    return [
+      {
+        label: 'Visio',
+        extensionLabel: '.vsdx',
+        canDownload: this.args.fileModel?.isVisioFile,
+        download: async () => {
+          this.onDownloadSuccess();
+        },
+        buttonSkin: 'primary',
+      },
+      {
+        label: 'BPMN',
+        extensionLabel: '.bpmn',
+        canDownload: this.args.fileModel?.isBpmnFile,
+        download: async () => {
+          this.onDownloadSuccess();
+        },
+        buttonSkin: 'primary',
+      },
+      {
+        label: 'Afbeelding',
+        extensionLabel: '.png',
+        canDownload: this.fileAsSVG,
+        download: async () => {
+          this.onDownloadSuccess();
+        },
+        buttonSkin: 'secondary',
+      },
+      {
+        label: 'Vectorafbeelding',
+        extensionLabel: '.svg',
+        canDownload: this.fileAsSVG,
+        download: async () => {
+          this.onDownloadSuccess();
+        },
+        buttonSkin: 'secondary',
+      },
+      {
+        label: 'PDF',
+        extensionLabel: '.pdf',
+        canDownload: true,
+        download: async () => {
+          this.onDownloadSuccess();
+        },
+        buttonSkin: 'secondary',
+      },
+    ];
+  }
+
+  get fileAsSVG() {
+    // TODO - transform to svg
+    return true;
+  }
+
+  onDownloadSuccess() {
+    this.toaster.success('Bestand werd succesvol gedownload', null, {
+      timeOut: 2500,
+    });
+    this.args.onDownloadSuccesful?.();
   }
 }
