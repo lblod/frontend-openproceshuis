e90e6083e8bfbbba87a24984f2ad8e72791c5159
Jonas Van Hoof <36266973+JonasVanHoof@users.noreply.github.com>
2026-02-12 09:31:00 +0100

    OPH-903 | Bring back download functionality for diagrams (#171)
    * refactor: models are updated + new list & listItem model
    
    Refs: oph-860
    
    * feat: can replace a diagram of an existing process
    
    Refs: oph-860
    
    * Oph-763 | Upload multiple diagrams (#165)
    
    * feat: create new page where we show all diagrams
    
    Refs: oph-861
    
    * feat: create file viewer + metadata-card component
    
    Refs: oph-861
    
    * feat: show table with diagram files in diagram-list
    
    Refs: oph-861
    
    * feat: make the title of the diagram file in the diagram-list clickable
    
    Refs: oph-861
    
    * feat: show a lock on older versions and you can only edit the current one
    
    Refs: oph-861
    
    * refactor: remove tests that are not used
    
    Refs: oph-861
    
    * refactor: creating a new process can now be done with multiple diagram files
    
    Refs : oph-763
    
    * refactor: update diagrams on a process (vervang) is also possible
    
    This with the bug that the latest diagram is not loaded in the viewer without a refresh
    
    Refs: oph-763
    
    * fix: the sorting of the diagram lists
    
    Refs: oph-763
    
    * fix: diagram file not loaded on process page because of filtering
    
    Refs: oph-763
    
    * fix: when removed a file from the list it will re open the lists first diagramfile
    
    Refs: oph-763
    
    * fix: the showcased file is the same as the first on the diagram list overview page (by position)
    
    Refs: oph-763
    
    * fix: fetch latest list when upload is done
    
    Refs: ph-763
    
    * refactor: extract the bpmn elements again on upload of files
    
    Refs: oph-763
    
    * Oph-781 | Add tabs for multiple diagrams (#168)
    
    * feat: create component to show the diagrams in tabs
    
    Refs: oph-781
    
    * refactor: diagrams page to use the new custom diagram classes
    
    Refs: oph-781
    
    * refactor: use the diagrams as tabs component instead of process:visual on process detail page
    
    Refs: oph-781
    
    * feat: make the scrollbar thinner
    
    Refs: oph-781
    
    * refactor: move the header out of the diagrams as tabs component
    
    Refs: oph-781
    
    * refactor: make breadcrum of diagrams page "diagrammen"
    
    Refs: oph-781
    
    * fix: linting issue in pipeline => comments in css file
    
    Refs: oph-781
    
    * Oph 781 bring back the replace button for diagrams (#169)
    
    * feat: show a big button to create a new diagram version at the top of the diagrams page
    
    Refs: oph-781
    
    * feat: On clicking new version it opens the replace diagrams modal
    
    Refs: oph-781
    
    * feat: show a banner when not looking at the latest diagrams + add cursor pointer when on tabs
    
    Refs: oph-781
    
    * refactor: no need to fetch latest when adding a new file
    
    Refs: oph-781
    
    * refactor: make the create version button the same style as on the other pages
    
    Refs: oph-781
    
    * feat: create base download file model component
    
    Refs: oph-903
    
    * refactor: pressing the download button of a diagram in table will open the download modal
    
    Refs: oph-903
    
    * refactor: move the diagrams table to the top of the page instead of under the diagrams visualizer
    
    Refs: oph-903
    
    * feat: add the diagram download logic for formats to the component
    
    Refs: oph-903
    
    * fix: can download visio and bpmn files
    
    Refs: oph-903
    
    * feat: the download model can also download as png,pdf and svg
    
    Refs: oph-903
    
    * feat: add loading state for the downloading of the file
    
    Refs: oph-903
    
    * fix: instead of the bpm container use a class that will always exist in the app
    
    Refs: oph-903
    
    * refactor: also show bpmn download when visio file
    
    Visio files can also be downloaded as bpmn
    
    Refs: oph-903
    
    * fix: can download visio as pdf again
    
    Refs: oph-903
    
    * fix: limit download png and svg to only bpmn files
    
    Refs: oph-903
    
    * feat: add tracking of downloaded files to the download file component
    
    Refs: oph-903
    
    * refactor: remove size and page for diagrams table
    
    Refs: oph-903
    
    * refactor: show the diagram versions sidebar on the right (like we did with ICR) (#173)
    
    Refs: oph-918

--- app/components/diagram-list/table.js
+++ app/components/diagram-list/table.js
@@ -10,10 +10,10 @@ import { getMessageForErrorCode } from 'frontend-openproceshuis/utils/error-mess
 
 export default class DiagramListTable extends Component {
   @service toaster;
-  page = 0;
-  size = 5;
-  sort = '-created';
+  @service store;
+  @service plausible;
 
+  @tracked fileToDownload;
   @tracked fileToDelete;
   @tracked isDeleteModalOpen;
   @tracked canDeleteFile = true;
@@ -67,4 +67,76 @@ export default class DiagramListTable extends Component {
 
     this.closeDeleteModal();
   });
+
+  @action
+  onFileDownloadedSuccessful(fileModel, targetExtension) {
+    this.fileToDelete = null;
+    this.trackDownloadFileEvent(
+      fileModel.id,
+      fileModel.name,
+      fileModel.extension,
+      targetExtension,
+      this.args.process,
+    );
+    this.incrementFileDownloads.perform(targetExtension, this.args.process?.id);
+  }
+
+  trackDownloadFileEvent(
+    fileId,
+    fileName,
+    fileExtension,
+    targetExtension,
+    process,
+  ) {
+    try {
+      this.plausible.trackEvent('Download bestand', {
+        'Bestand-ID': fileId,
+        Bestandsnaam: fileName,
+        Bestandstype: fileExtension,
+        Downloadtype: targetExtension ?? fileExtension,
+        'Proces-ID': process?.id,
+        Procesnaam: process?.title,
+        'Bestuur-ID': process?.publisher?.id,
+        Bestuursnaam: process?.publisher?.name,
+      });
+    } catch (error) {
+      console.error(
+        `Something went wrong while trying to fetch the download quantity of ${targetExtension} `,
+        error,
+      );
+    }
+  }
+
+  incrementFileDownloads = task(
+    { enqueue: true },
+    async (targetExtension, processId) => {
+      try {
+        const process = await this.store.findRecord('process', processId);
+        const stats = process.processStatistics;
+        switch (targetExtension) {
+          case 'bpmn':
+            stats.bpmnDownloads += 1;
+            break;
+          case 'pdf':
+            stats.pdfDownloads += 1;
+            break;
+          case 'png':
+            stats.pngDownloads += 1;
+            break;
+          case 'svg':
+            stats.svgDownloads += 1;
+            break;
+          default:
+            console.error('fileExtension', targetExtension, 'not recognized');
+            return;
+        }
+        await stats.save();
+      } catch (error) {
+        console.error(
+          `Something went wrong while loading the file downloads:`,
+          error,
+        );
+      }
+    },
+  );
 }
