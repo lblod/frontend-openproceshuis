4c41e0314b0494b55a6af9a94d27542dcd06b06c
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2026-02-18 13:01:54 +0100

    refactor: just use the transition.parent.name for the modelFor method
    Refs: oph-921

--- app/routes/processes/process/index.js
+++ app/routes/processes/process/index.js
@@ -6,7 +6,7 @@ export default class ProcessesProcessIndexRoute extends Route {
   @service store;
 
   async model(_params, transition) {
-    const { id } = this.modelFor(this.getModelNameForRoute(transition));
+    const { id } = this.modelFor(transition.to.parent.name);
     const process = await this.store.findRecord('process', id);
     let stats = process.processStatistics;
 
@@ -44,27 +44,5 @@ export default class ProcessesProcessIndexRoute extends Route {
       const el = document.getElementById(scrollTo);
       if (el) el.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
-  }
-
-  getModelNameForRoute(transition) {
-    const routeMap = [
-      {
-        testString: 'shared-processes',
-        routeName: 'shared-processes.process',
-      },
-      {
-        testString: 'my-local-government',
-        routeName: 'my-local-government.process',
-      },
-    ];
-
-    const toRouteName = transition?.to?.name;
-    for (const route of routeMap) {
-      if (toRouteName && toRouteName.includes(route.testString)) {
-        return route.routeName;
-      }
-    }
-
-    return 'processes.process';
   }
 }
