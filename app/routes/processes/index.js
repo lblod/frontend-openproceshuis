e324bc637178ff76cf70561e13fb1fca7fe1a0d8
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2026-04-20 16:54:43 +0200

    refactor: transform fetch process from action to task
    Refs: oph-1031

--- app/routes/processes/index.js
+++ app/routes/processes/index.js
@@ -1,5 +1,5 @@
 import Route from '@ember/routing/route';
-import { keepLatestTask } from 'ember-concurrency';
+import { task } from 'ember-concurrency';
 import { service } from '@ember/service';
 import ENV from 'frontend-openproceshuis/config/environment';
 
@@ -30,67 +30,70 @@ export default class ProcessesIndexRoute extends Route {
     };
   }
 
-  @keepLatestTask({ cancelOn: 'deactivate' })
-  *loadProcessesTask(params) {
-    let query = {
-      page: {
-        number: params.page,
-        size: params.size,
-      },
-      include: [
-        'publisher',
-        'creator',
-        'users',
-        'publisher.primary-site',
-        'publisher.primary-site.contacts',
-        'publisher.classification',
-        'relevant-administrative-units',
-        'ipdc-products',
-      ].join(','),
-    };
+  loadProcessesTask = task(
+    { keepLatest: true, cancelOn: 'deactivate' },
+    async (params) => {
+      let query = {
+        page: {
+          number: params.page,
+          size: params.size,
+        },
+        include: [
+          'publisher',
+          'creator',
+          'users',
+          'publisher.primary-site',
+          'publisher.primary-site.contacts',
+          'publisher.classification',
+          'relevant-administrative-units',
+          'ipdc-products',
+        ].join(','),
+      };
 
-    if (params.sort) {
-      const isDescending = params.sort.startsWith('-');
+      if (params.sort) {
+        const isDescending = params.sort.startsWith('-');
 
-      let fieldName = isDescending ? params.sort.substring(1) : params.sort;
+        let fieldName = isDescending ? params.sort.substring(1) : params.sort;
 
-      if (fieldName === 'organization') fieldName = 'publisher.name';
-      else if (fieldName === 'classification')
-        fieldName = 'publisher.classification.label';
+        if (fieldName === 'organization') fieldName = 'publisher.name';
+        else if (fieldName === 'classification')
+          fieldName = 'publisher.classification.label';
 
-      let sortValue = `:no-case:${fieldName}`;
-      if (isDescending) sortValue = `-${sortValue}`;
+        let sortValue = `:no-case:${fieldName}`;
+        if (isDescending) sortValue = `-${sortValue}`;
 
-      query.sort = sortValue;
-    }
+        query.sort = sortValue;
+      }
 
-    if (params.title) {
-      query['filter[:or:][title]'] = params.title;
-      query['filter[:or:][description]'] = params.title;
-    }
+      if (params.title) {
+        query['filter[:or:][title]'] = params.title;
+        query['filter[:or:][description]'] = params.title;
+      }
 
-    if (params.modifiedSince) {
-      query['filter[:gte:modified]'] = params.modifiedSince;
-    }
+      if (params.modifiedSince) {
+        query['filter[:gte:modified]'] = params.modifiedSince;
+      }
 
-    if (params.classifications) {
-      query['filter[relevant-administrative-units][:id:]'] =
-        params.classifications;
-    }
+      if (params.classifications) {
+        query['filter[relevant-administrative-units][:id:]'] =
+          params.classifications;
+      }
 
-    if (params.group) query['filter[publisher][:exact:name]'] = params.group;
-    if (params.creator) query['filter[creator][:exact:name]'] = params.creator;
+      if (params.group) query['filter[publisher][:exact:name]'] = params.group;
+      if (params.creator)
+        query['filter[creator][:exact:name]'] = params.creator;
 
-    if (params.blueprint) {
-      query['filter[is-blueprint]'] = params.blueprint;
-    }
+      if (params.blueprint) {
+        query['filter[is-blueprint]'] = params.blueprint;
+      }
 
-    if (params.ipdcProducts) {
-      query['filter[ipdc-products][:id:]'] = params.ipdcProducts;
-    }
+      if (params.ipdcProducts) {
+        query['filter[ipdc-products][:id:]'] = params.ipdcProducts;
+      }
 
-    query['filter[:not:status]'] = ENV.resourceStates.archived;
+      query['filter[:not:status]'] = ENV.resourceStates.archived;
 
-    return yield this.store.query('process', query);
-  }
+      return await this.store.query('process', query);
+    },
+  );
 }
