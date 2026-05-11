f1aad27120a7f7921942df659bc9d767cf150ec1
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2026-05-11 16:27:15 +0200

    refactor: update logic to also compare the array items by a property on depth 0
    Refs: oph-1022

--- app/utils/versioning-comparison.js
+++ app/utils/versioning-comparison.js
@@ -1,13 +1,34 @@
-export function isArrayDiverging(currentItems = [], comparisonItems = []) {
+export function isArrayDiverging(
+  currentItems = [],
+  comparisonItems = [],
+  propertiesToCheck,
+) {
   if (currentItems.length !== comparisonItems.length) {
     return true;
   }
 
   return currentItems.some((item, index) => {
-    return isDifferent(item, comparisonItems[index]);
+    return isDifferent(item, comparisonItems[index], propertiesToCheck);
   });
 }
 
-function isDifferent(value, comparedValue) {
-  return value !== comparedValue;
+// NOTE: This is only checking on depth 0
+function isDifferent(value, comparedValue, propertiesToCheck) {
+  const propertiesToBeEqual =
+    propertiesToCheck?.length >= 1 ? propertiesToCheck : ['id'];
+  if (value === comparedValue) {
+    return false;
+  }
+
+  const isValueAnObject = value && typeof value === 'object';
+  const isComparedValueAnObject =
+    comparedValue && typeof comparedValue === 'object';
+
+  if (isValueAnObject && isComparedValueAnObject) {
+    return propertiesToBeEqual.some(
+      (prop) => value[prop] !== comparedValue[prop],
+    );
+  }
+
+  return true;
 }
--- tests/unit/utils/versioning-comparison-test.js
+++ tests/unit/utils/versioning-comparison-test.js
@@ -6,24 +6,37 @@ module('Unit | Utility | versioning-comparison', function () {
     const current = [];
     const compared = ['different'];
     let isDiverging = isArrayDiverging(current, compared);
-    assert.true(isDiverging, 'length is different so the arrays are diverging');
+    assert.true(isDiverging);
   });
-  test('it IS NOT diverging when array values are the same ', function (assert) {
+  test('it checks when simple array values are the same ', function (assert) {
     const current = ['value', 3, true];
     const compared = ['value', 3, true];
-    let isEqualArrayDiverging = isArrayDiverging(current, compared);
-    assert.false(
-      isEqualArrayDiverging,
-      'not diverging when array values are EQUAL',
-    );
+    let isDiverging = isArrayDiverging(current, compared);
+    assert.false(isDiverging);
   });
-  test('it IS diverging when array values are different', function (assert) {
+  test('it checks when simple array values are different', function (assert) {
     const current = ['value', '3', true];
     const compared = ['value', 3, true];
-    let isEqualArrayDiverging = isArrayDiverging(current, compared);
+    let isDiverging = isArrayDiverging(current, compared);
+    assert.true(isDiverging);
+  });
+  test('it checks the id if objects are in the array and not properties are defined to check on', function (assert) {
+    const modelA = { id: '101' };
+
+    let isDiverging = isArrayDiverging([modelA], [modelA]);
+    assert.false(isDiverging);
+  });
+  test('it checks specific properties for divergence', function (assert) {
+    const current = { id: '1', isArchived: true };
+    const compared = { id: '1', isArchived: false };
+
+    assert.false(
+      isArrayDiverging([current], [compared], ['id']),
+      'Not different when checking only id',
+    );
     assert.true(
-      isEqualArrayDiverging,
-      'diverging when array values are NOT EQUAL',
+      isArrayDiverging([current], [compared], ['isArchived']),
+      'Is different when checking the isArchived property',
     );
   });
 });
