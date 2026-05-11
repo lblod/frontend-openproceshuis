621a755ff6afaccf1e5bf2ac211cd4227652cebb
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2026-05-11 15:07:48 +0200

    refactor: remove all other tests that do not have content and fix running ember test command
    Refs: oph-1022

---
+++ tests/unit/utils/array-comparison-test.js
@@ -0,0 +1,10 @@
+import arrayComparison from 'frontend-openproceshuis/utils/array-comparison';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | array-comparison', function () {
+  // TODO: Replace this with your real tests.
+  test('it works', function (assert) {
+    let result = arrayComparison();
+    assert.ok(result);
+  });
+});
