50aaac213864d8521f5ec9a753b8e3eb524231d9
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2026-06-11 08:32:22 +0200

    feat: update Link model with created and modified + save updates modified
    Refs: oph-1099

--- app/models/link.js
+++ app/models/link.js
@@ -5,6 +5,14 @@ export default class LinkModel extends Model {
   @attr('string') label;
   @attr('string') href;
   @attr('string') status;
+  @attr('iso-date', {
+    default: new Date(),
+  })
+  created;
+  @attr('iso-date', {
+    default: new Date(),
+  })
+  modified;
 
   get displayLabel() {
     if (!this.label) {
@@ -15,5 +23,10 @@ export default class LinkModel extends Model {
 
   get isArchived() {
     return this.status === ENV.resourceStates.archived;
+  }
+
+  async save() {
+    this.modified = new Date();
+    await super.save(...arguments);
   }
 }
