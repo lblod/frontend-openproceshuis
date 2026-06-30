8010f2e8675b2526662c64fab16c717dc5d7c44c
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2026-06-30 12:08:28 +0200

    refactor: separate the dropzone and file list into a new component for the wizard (prep for tabs)
    Ref: oph-864

---
+++ app/components/wizard/add-diagrams.js
@@ -0,0 +1,21 @@
+import Component from '@glimmer/component';
+
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class WizardAddDiagrams extends Component {
+  @tracked fileWrappers = [];
+
+  @action
+  handleFileUpload(fileWrappers) {
+    this.fileWrappers = fileWrappers;
+    this.args.onFileWrappersChanged?.(fileWrappers);
+  }
+
+  @action
+  removeFileWrapper(fileWrapper) {
+    fileWrapper.queue.remove(fileWrapper);
+    this.fileWrappers = this.fileWrappers.filter((fw) => fw !== fileWrapper);
+    this.args.onFileWrappersChanged?.(this.fileWrappers);
+  }
+}
