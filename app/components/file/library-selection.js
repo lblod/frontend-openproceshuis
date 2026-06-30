ee95b24b704f5ed204e6548bb227f60d4575642a
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2026-06-30 15:20:51 +0200

    feat: show a search field + 5 results for the file search
    Refs: oph-864

---
+++ app/components/file/library-selection.js
@@ -0,0 +1,37 @@
+import Component from '@glimmer/component';
+
+import { A } from '@ember/array';
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { task, timeout } from 'ember-concurrency';
+import { ARCHIVED_STATUS_URI } from '../../utils/well-known-uris';
+
+export default class FileLibrarySelection extends Component {
+  @service store;
+
+  @tracked searchResults = A([]);
+
+  searchFile = task({ restartable: true }, async (event) => {
+    await timeout(250);
+    const inputValue = event.target?.value?.trim();
+    this.searchResults.clear();
+    const results = await this.store.query('file', {
+      'filter[name]': inputValue,
+      'filter[:not:status]': ARCHIVED_STATUS_URI,
+      sort: 'created',
+      page: {
+        number: 0,
+        size: 5,
+      },
+    });
+
+    this.searchResults.pushObjects(results);
+  });
+
+  @action
+  selectFile(_file, action) {
+    console.log(_file, action);
+    this.args.onFileSelected?.(_file);
+  }
+}
