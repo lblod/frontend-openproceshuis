d676752f1f810aeea0f1b302fd0e304295d6146e
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2026-08-26 09:03:53 +0200

    feat: can initially collapse data-card
    Default behavior is still open
    
    Refs: oph-1161

--- app/components/data-card.js
+++ app/components/data-card.js
@@ -5,6 +5,11 @@ import { tracked } from '@glimmer/tracking';
 export default class DataCard extends Component {
   @tracked isCollapsed = false;
 
+  constructor() {
+    super(...arguments);
+    this.isCollapsed = this.args.isInitiallyClosed ?? false;
+  }
+
   @action
   toggleCollapse() {
     this.isCollapsed = !this.isCollapsed;
